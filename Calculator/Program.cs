ce2f44541ec3ef2f064e802fd787bfda648a603a
Oleksandr <alasdair.p25@gmail.com>
2022-02-10 20:05:29 +0200

    New projekt calk

---
+++ Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Project__hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+            double firstValue, secondValue;
+            string action;
+
+            Console.WriteLine("Введiть число 1");
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введiть число 2");
+            secondValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Виберiть операцiю: '+' '-' '*' '/'");
+            action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "+":
+                    Console.WriteLine(firstValue + secondValue);
+                    break;
+                case "-":
+                    Console.WriteLine(firstValue - secondValue);
+                    break;
+                case "*":
+                    Console.WriteLine(firstValue * secondValue);
+                    break;
+                case "/":
+                    if (secondValue == 0)
+                    {
+                        Console.WriteLine("Помилка!");
+                    }
+                    else
+                    {
+                    Console.WriteLine(firstValue / secondValue);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Помилка! Невiдома дiя");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
