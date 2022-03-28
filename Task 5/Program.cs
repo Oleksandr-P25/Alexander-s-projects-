3b1b5a0d4f2f69c8d74ed7392c8956702cad38ef
Oleksandr <alasdair.p25@gmail.com>
2022-03-28 21:18:53 +0300

    Task 5 bronze, silver

---
+++ Task 5/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Person
+    {
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+
+        public void PrintName()
+        {
+            Console.WriteLine($"Мене звати {FirstName}");
+        }
+    }
+
+    class Store_employee : Person
+    {
+        public void Cashier()
+        {
+            Console.WriteLine("Я вмiю пробивати товари");
+        }
+
+        public void Accountant()
+        {
+            Console.WriteLine("Я вмiю рахувати кошти");
+        }
+
+        public void Manager()
+        {
+            Console.WriteLine("Я вмiю робити звiти");
+        }
+    }
+
+    class Client : Person
+    {
+        public void Shopper()
+        {
+            Console.WriteLine("Я вмiю купувати товари");
+        }
+    }
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Store_employee employee = new Store_employee();
+            employee.FirstName = "Alex";
+            employee.PrintName();
+            employee.Cashier();
+       
+            Store_employee employee1 = new Store_employee();
+            employee1.FirstName = "Oleg";
+            employee1.PrintName();
+            employee1.Manager();
+
+            Client client1 = new Client();
+            client1.FirstName = "Anna";
+            client1.PrintName();
+            client1.Shopper();
+
+            Console.ReadLine();
+        }
+    }
+}
